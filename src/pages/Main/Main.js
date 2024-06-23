682160093573842dc0d09a96a48491d762ee7071
jaewoong <wodnd0284@naver.com>
2024-06-23 16:24:47 +0900

    Nav,Footer 레이아웃 완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,14 @@
-import "./Main.scss"
+import Nav from "../../components/Nav/Nav";
+import Footer from "../../components/Footer/Footer";
+import "./Main.scss";
 
-const Main = () => { 
-  return(
-    <div></div>
-  )
- }
+const Main = () => {
+  return (
+    <div>
+      <Nav />
+      <Footer />
+    </div>
+  );
+};
 
 export default Main;
