a11388c72636f63252e5ee0aee175d1a4b609218
jaewoong <wodnd0284@naver.com>
2024-07-04 17:42:04 +0900

    Feat : useParams를 사용한 API요청 및 영화 추천 기능 추가, 반응형 웹

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./Main.scss";
 import MovieSlider from "../../components/MovieSlider/MovieSlider";
 
@@ -12,20 +12,24 @@ const Main = () => {
   useEffect(() => {
     const fetchFeaturedMovie = async () => {
       try {
-        const popularResponse = await fetch(`${baseURL}/movie/popular?api_key=${key}&language=ko-KR&page=1`);
+        const popularResponse = await fetch(
+          `${baseURL}/movie/popular?api_key=${key}&language=ko-KR&page=1`
+        );
         const popularData = await popularResponse.json();
-        console.log(popularData)
-        
+        console.log(popularData);
+
         for (let movie of popularData.results) {
-          const videoResponse = await fetch(`${baseURL}/movie/${movie.id}/videos?api_key=${key}&language=ko-KR`);
+          const videoResponse = await fetch(
+            `${baseURL}/movie/${movie.id}/videos?api_key=${key}&language=ko-KR`
+          );
           const videoData = await videoResponse.json();
-          
+
           if (videoData.results && videoData.results.length > 0) {
-            console.log(videoData)
+            console.log(videoData);
             const videoKey = videoData.results[0].key;
             setFeaturedMovie({
               ...movie,
-              videoUrl: `https://www.youtube.com/embed/${videoKey}`
+              videoUrl: `https://www.youtube.com/embed/${videoKey}`,
             });
             break;
           }
@@ -52,14 +56,13 @@ const Main = () => {
             <iframe
               src={featuredMovie.videoUrl}
               title={featuredMovie.title}
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
           ) : (
-            <img 
-              src={`https://image.tmdb.org/t/p/original${featuredMovie.backdrop_path}`} 
-              alt={featuredMovie.title} 
+            <img
+              src={`https://image.tmdb.org/t/p/original${featuredMovie.backdrop_path}`}
+              alt={featuredMovie.title}
             />
           )}
           <div className="video-content">
@@ -74,7 +77,7 @@ const Main = () => {
           Latest Movie | <span className="watch">Watch All</span>
         </p>
       </div>
-      <MovieSlider endpoint="now_playing" title="최신 영화" />
+      <MovieSlider endpoint="now_playing" />
     </>
   );
 };
